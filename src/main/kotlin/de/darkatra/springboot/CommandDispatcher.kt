79be1cf1f76624b4f927364d0a25f1df134573b3
Tommy Schmidt <tommy.schmidt@idealo.de>
2026-05-09 19:09:57 +0200

    feat: add support for valiating commands before they are dispatched to their CommandHandler

--- src/main/kotlin/de/darkatra/springboot/CommandDispatcher.kt
+++ src/main/kotlin/de/darkatra/springboot/CommandDispatcher.kt
@@ -1,19 +1,40 @@
 package de.darkatra.springboot
 
 /**
- * Dispatches commands to their corresponding command handlers, ensuring that each command is processed
- * by the appropriate handler registered in the system. This class serves as the central mechanism for
- * executing commands and managing the relationship between commands and their handlers.
+ * Dispatches commands to their corresponding [CommandHandler] and optionally validates them using the associated [CommandValidator].
+ *
+ * @param commandToCommandHandlerMap A map that associates each command type with its corresponding handler.
+ * @param commandToCommandValidatorMap A map that associates each command type with its corresponding validator.
  */
 class CommandDispatcher internal constructor(
-    private val commandToCommandHandlerMap: Map<Class<out Command<*>>, CommandHandler<out Command<*>, *>> = emptyMap()
+    private val commandToCommandHandlerMap: Map<Class<out Command<*>>, CommandHandler<out Command<*>, *>> = emptyMap(),
+    private val commandToCommandValidatorMap: Map<Class<out Command<*>>, CommandValidator<out Command<*>, *>> = emptyMap(),
 ) {
 
+    /**
+     * Dispatches a given [Command] to its associated [CommandHandler] and optionally validates it using the corresponding [CommandValidator].
+     *
+     * @param command The command to dispatch.
+     * @return The result of the command.
+     * @throws CommandValidationException If the command validation fails or an exception occurs during validation.
+     * @throws IllegalStateException If no command handler is found for the provided command.
+     */
     fun <C : Command<R>, R> dispatch(command: C): R {
 
         @Suppress("UNCHECKED_CAST")
+        val commandValidator = commandToCommandValidatorMap[command.javaClass] as CommandValidator<C, R>?
+        try {
+            commandValidator?.validate(command)
+        } catch (e: Exception) {
+            if (e is CommandValidationException) {
+                throw e
+            }
+            throw CommandValidationException("Exception validating Command: ${command.javaClass.simpleName}", command, e)
+        }
+
+        @Suppress("UNCHECKED_CAST")
         val commandHandler = commandToCommandHandlerMap[command.javaClass] as CommandHandler<C, R>?
-            ?: throw IllegalStateException("No CommandHandler found for Command: ${command.javaClass}")
+            ?: throw IllegalStateException("No CommandHandler found for Command: ${command.javaClass.simpleName}")
 
         return commandHandler.handle(command)
     }
--- src/main/kotlin/de/darkatra/springboot/CommandHandlerAutoConfiguration.kt
+++ src/main/kotlin/de/darkatra/springboot/CommandHandlerAutoConfiguration.kt
@@ -6,7 +6,8 @@ import org.springframework.core.GenericTypeResolver
 
 @AutoConfiguration
 class CommandAutoConfiguration(
-    private val commandHandlers: List<CommandHandler<out Command<*>, *>>
+    private val commandHandlers: List<CommandHandler<out Command<*>, *>>,
+    private val commandValidators: List<CommandValidator<out Command<*>, *>>,
 ) {
 
     @Bean
@@ -24,13 +25,35 @@ class CommandAutoConfiguration(
                 @Suppress("UNCHECKED_CAST")
                 val commandType = generics[0] as Class<out Command<*>?>
                 check(!containsKey(commandType)) {
-                    "Commands can only have one CommandHandler. There was an attempt to register an additional CommandHandler for Command: ${commandType.getSimpleName()}"
+                    "Each Command can have at most one CommandHandler. There was an attempt to register an additional CommandHandler for Command: ${commandType.getSimpleName()}"
                 }
 
                 put(commandType, commandHandler)
             }
         }
 
-        return CommandDispatcher(commandToCommandHandlerMap)
+        val commandToCommandValidatorMap: Map<Class<out Command<*>>, CommandValidator<out Command<*>, *>> = buildMap {
+            commandValidators.forEach { commandValidator ->
+
+                // resolve the generics for the CommandValidator interface
+                val generics = GenericTypeResolver.resolveTypeArguments(commandValidator.javaClass, CommandValidator::class.java)
+                check(!(generics == null || generics.size != 2)) {
+                    "Expected CommandValidator interface to have exactly two generics. Found: ${generics.contentToString()}"
+                }
+
+                @Suppress("UNCHECKED_CAST")
+                val commandType = generics[0] as Class<out Command<*>?>
+                check(!containsKey(commandType)) {
+                    "Each Command can have at most one CommandValidator. There was an attempt to register an additional CommandValidator for Command: ${commandType.getSimpleName()}"
+                }
+
+                put(commandType, commandValidator)
+            }
+        }
+
+        return CommandDispatcher(
+            commandToCommandHandlerMap = commandToCommandHandlerMap,
+            commandToCommandValidatorMap = commandToCommandValidatorMap,
+        )
     }
 }
